d87aa048664b41ca426a087190ef44135c163005
Aman Kumar <amank2046@gmail.com>
2026-04-20 21:36:16 +0530

    Initial server setup and .gitignore

---
+++ server/src/app.js
@@ -0,0 +1,37 @@
+import dotenv from 'dotenv';
+dotenv.config({path: "./.env"});
+
+import createError from 'http-errors';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import morgan from "morgan";
+
+export const app = express();
+export const port = process.env.PORT || 4000;
+
+app.set("view engine", "ejs")
+
+app.use(cors());
+
+app.use(express.json({limit: "50mb"}));
+app.use(express.urlencoded({extended: true, limit: "50mb"}));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(morgan("dev")); //HTTP request logger middleware for node.js
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+});
+
+// error handler
+// app.use(function (err, req, res, next) {
+//     // set locals, only providing error in development
+//     res.locals.message = err.message;
+//     res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+//     // render the error page
+//     res.status(err.status || 500);
+//     res.render('error');
+// });
