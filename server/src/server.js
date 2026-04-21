22ac15621285cf535c2828075f7898b3371937d1
Aman Kumar <amank2046@gmail.com>
2026-04-21 22:17:13 +0530

    Add client React + Vite frontend scaffold

--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,13 @@
 import connectToDB from './dataBase/index.js';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 import {app, port} from './app.js';
-dotenv.config({path: "./.env"});
+// dotenv.config({path: ".env"});
 
 connectToDB()
 .then(()=>{
-    // app.get("/", (req, res)=>{
-    //     res.send("Hello World!!");
-    // });
+    app.get("/", (req, res)=>{
+        res.send("Hello World!!");
+    });
     app.listen(port, ()=>{
         console.log("Server is running on port: ", port);
     });
