372dbaeffae733a9993d7ee0dd18150892366641
Aman Kumar <118505622+Amankumar-02@users.noreply.github.com>
2026-04-22 22:27:20 +0530

    Update constants.js

--- server/src/constants.js
+++ server/src/constants.js
@@ -1,1 +1,19 @@
 export const DB_NAME = "CRUD_OPERATIONS_E2E";
+
+export const userIds = [
+  { id: 0, name: "User0", age: 20, email: "user0@example.com", isActive: true },
+  { id: 1, name: "User1", age: 21, email: "user1@example.com", isActive: false },
+  { id: 2, name: "User2", age: 22, email: "user2@example.com", isActive: true },
+  { id: 3, name: "User3", age: 23, email: "user3@example.com", isActive: false },
+  { id: 4, name: "User4", age: 24, email: "user4@example.com", isActive: true },
+  { id: 5, name: "User5", age: 25, email: "user5@example.com", isActive: false },
+  { id: 6, name: "User6", age: 26, email: "user6@example.com", isActive: true },
+  { id: 7, name: "User7", age: 27, email: "user7@example.com", isActive: false },
+  { id: 8, name: "User8", age: 28, email: "user8@example.com", isActive: true },
+  { id: 9, name: "User9", age: 29, email: "user9@example.com", isActive: false },
+  { id: 10, name: "User10", age: 30, email: "user10@example.com", isActive: true },
+  { id: 11, name: "User11", age: 31, email: "user11@example.com", isActive: false },
+  { id: 12, name: "User12", age: 32, email: "user12@example.com", isActive: true },
+  { id: 13, name: "User13", age: 33, email: "user13@example.com", isActive: false },
+  { id: 14, name: "User14", age: 34, email: "user14@example.com", isActive: true }
+];
