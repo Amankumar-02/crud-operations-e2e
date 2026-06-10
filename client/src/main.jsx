e70d466bb3c62e98b0f9d4a9ac19bdcaa6269245
Aman Kumar <amank2046@gmail.com>
2026-06-10 23:59:31 +0530

    Add routing, Redux deps

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router";
+import App from "./App.jsx";
+import About from "./pages/About.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<App/>}>
+      <Route path='/about' element={<About/>}/>
+    </Route>
+  )
 )
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <BrowserRouter>
+      <RouterProvider router={router} />
+    </BrowserRouter>
+  </StrictMode>,
+);
